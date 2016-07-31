fb08d6dc10250ad5d522d0e13fc30d5e190fee92
abharath27 <abharath27@users.noreply.github.com>
2016-07-30 20:48:49 -0500

    Created by Coding Ground Client

---
+++ New Project-20160730/main.py
@@ -0,0 +1,28 @@
+from collections import OrderedDict
+
+s = "adobecodebanchellothereaadsbcalskjd;fas"
+t = "abc"
+
+d = OrderedDict()
+windowStart = -1
+windowEnd = -1
+windowSize = -1
+
+for i in range(len(s)):
+    if s[i] in t:
+        if s[i] in d:
+            del d[s[i]]
+        d[s[i]] = i
+        print d
+        if len(d) == len(t):
+            tempStart = next(iter(d.values()))
+            tempEnd = next(reversed(d.values()))
+            #print windowStart
+            #print windowEnd
+            if windowSize == -1 or windowSize > (tempEnd - tempStart):
+                windowSize = tempEnd - tempStart
+                windowStart = tempStart
+                windowEnd = tempEnd
+            print [windowSize, windowStart, windowEnd]
+    
+print s[windowStart:windowEnd+1]    
