ff8217f2d85383084cac1db94c7c397bb4cb1acf
abharath27 <abharath27@users.noreply.github.com>
2016-07-30 18:50:08 -0700

    Update main.py

--- New Project-20160730/main.py
+++ New Project-20160730/main.py
@@ -1,3 +1,8 @@
+#Given a random string S and another string T with unique elements, find the minimum consecutive sub-string of S such that it contains all the elements in T. 
+#example: 
+#S='adobecodebanc' 
+#T='abc' 
+#answer='banc'
 from collections import OrderedDict
 
 s = "adobecodebanchellothereaadsbcalskjd;fas"
